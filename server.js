c32aef09c56965a0ba016c68967058530da30b49
vipul <itsvipulkumar02@gmail.com>
2026-01-28 21:43:52 +0530

    code checking : 21432802

--- server.js
+++ server.js
@@ -48,3 +48,4 @@ app.use("/api/chat", require("./routes/chatRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+console.log("code checking : 21432802")
