bddc7dccb8e31c8f1d057ee01130afa2737659da
vipul <itsvipulkumar02@gmail.com>
2026-02-03 23:18:09 +0530

    diabeteswellness updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
       "https://diabeteswellnes.online",
       "https://www.diabeteswellnes.online",
       "https://diabeteswellnes.health",
-      "https://www.diabeteswellnes.health",
+      "https://www.diabeteswellness.health",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
