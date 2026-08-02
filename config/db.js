c0f45a971d1bd965d75979f9cc4e22c25b6a8f09
vipul <itsvipulkumar02@gmail.com>
2026-08-02 09:59:20 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+// Node picks up a stale/unreachable 127.0.0.1 resolver on this machine, which
+// breaks the SRV lookup mongodb+srv:// needs (ECONNREFUSED). Force a working
+// resolver so the Atlas SRV record can be queried.
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
